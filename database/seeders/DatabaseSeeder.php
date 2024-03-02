0eeec63870ab474ddc1d4d7aa2fe961f9ad516b3
DimasSetiady16 <dimasaasetiady@gmail.com>
2024-03-02 12:23:54 +0700

    Tugas2_CRUD_011

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+         \App\Models\User::factory(10)->create();
+
+         \App\Models\User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+             'password' => bcrypt('12345678'),
+         ]);
+         
+         DB::table('kategori')->insert([
+            'nama_kategori' => 'Nasional'
+         ]);
+
+         DB::table('kategori')->insert([
+            'nama_kategori' => 'Internasional'
+         ]);
+    }
+}
